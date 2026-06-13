27e85b949093168c7e3ffef1a56c8f03a99c5a0a
Svitlana Pushchalo <svitlana8@gmail.com>
2026-06-13 19:25:53 +0300

    endpoint products/matching_toRemove

--- tests/test_products_matching_toRemove.py
+++ tests/test_products_matching_toRemove.py
@@ -5,16 +5,35 @@ from lib.base_case import BaseCase
 
 class TestEPM(BaseCase):
     """sent_productID, sent_typeId, target_zipCode, target_storeId, target_materialId, target_materialName, target_colorId, target_productQteGrpId, retailer"""
-    parametersList = [
-    (39091, 1111, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
-    (40926, 1102, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
-    (40921, 1102, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
-    (30692, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
-    (39114, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
-    (39112, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
-    (40919, 1106, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
-    ]
-
+    parametersList = [(39091, 1111, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
+                      (40926, 1102, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
+                      (40921, 1102, "07652", 25922, 2510, "Quartz", 0, 740, 'fd'),
+                      (30692, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
+                      (39114, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
+                      (39112, 1111, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
+                      (40919, 1106, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
+                      (40924, 1106, "07652", 25922, 2500, "Natural Stone", 0, 780, 'fd'),
+                      (35938, 1102, "45245", 26198, 2513, "Solid Surface", 0, 780, 'homeoutlet'),
+                      (35958, 1107, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (36132, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (36134, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (35963, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40911, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (35958, 1107, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (35959, 1107, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40906, 1107, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (35957, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (36134, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (35963, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40284, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40285, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40286, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40287, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (40288, 1111, "45245", 26198, 2513, "Solid Surface", 0, 750, 'homeoutlet'),
+                      (37479, 1102, "45245", 26198, 2500, "Natural Stone", 0, 780, 'homeoutlet'),
+                      (40939, 1111, "45245", 26198, 2500, "Natural Stone", 0, 780, 'homeoutlet'),
+                      (37495, 1107, "45245", 26198, 2500, "Natural Stone", 0, 780, 'homeoutlet')
+                      ]
 
     def get_payload(self,
                     product_id,
@@ -56,12 +75,14 @@ class TestEPM(BaseCase):
             sent_product_id, sent_type_id, zip_code, store_id, material_id, material_name, color_id, qte_grp_id
         )
 
-        response = requests.post(f"{self.base_url}products/matching", json=current_payload, headers={"Authorization": self.tokens_list.get(retailer)})
+        response = requests.post(f"{self.base_url}products/matching", json=current_payload,
+                                 headers={"Authorization": self.tokens_list.get(retailer)})
         assert response.status_code == 200, f"Wrong status code - 200 is expected, but got {response.status_code}: {response.reason}"
 
         response_json = response.json()
 
-        assert len(response_json["productsToRemove"]) != 0, f"Mapping error! Expected productID: {sent_product_id} to be removed, but no product returned to remove."
+        assert len(response_json[
+                       "productsToRemove"]) != 0, f"Mapping error! Expected productID: {sent_product_id} to be removed, but no product returned to remove."
         assert "productId" in response_json["productsToRemove"][0], "Response JSON does not contain 'productId' field"
         # Отримуємо newProductID, який повернув сервер
         received_product_id = response_json["productsToRemove"][0]["productId"]
