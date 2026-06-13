6745ed5a83783fc1e5c0cde10cfc07ea6a9132eb
Svitlana Pushchalo <svitlana8@gmail.com>
2026-06-13 17:27:53 +0300

    endpoint /setup

--- lib/base_case.py
+++ lib/base_case.py
@@ -6,13 +6,30 @@ from requests import Response
 
 class BaseCase:
     base_url = "https://ee-api-ssi.staging.inscyth.com/"
+    tokens_list = {}
 
     def setup_class(self):
-        retailer = {'clientHost': 'fd.staging.inscyth.com'}
+        # retailer = {'clientHost': 'fd.staging.inscyth.com'}
+        #
+        # response = requests.get("https://ee-api-ssi.staging.inscyth.com/lookup/retailer", params = retailer)
+        # assert response.status_code == 200, 'Wrong status code'
+        #
+        # assert "token" in response.json(), "There is no Bearer token in the response_"
+        # self.token = response.json().get("token")
 
-        response_ = requests.get("https://ee-api-ssi.staging.inscyth.com/lookup/retailer", params=retailer)
-        assert response_.status_code == 200, 'Wrong status code'
+        retailers = ['fd.staging.inscyth.com', 'homeoutlet.staging.inscyth.com', 'lowes.staging.inscyth.com']
 
-        assert "token" in response_.json(), "There is no Bearer token in the response_"
-        self.token = response_.json().get("token")
+        for item in retailers:
+            response = requests.get(f"{self.base_url}lookup/retailer", params={'clientHost': item})
+
+            assert response.status_code == 200, 'Wrong status code'
+
+            assert "token" in response.json(), "There is no Bearer token in the response_"
+            token = response.json().get("token")
+
+            assert "retailerCode" in response.json(), "There is no retailerCode in the response_"
+            raw_retailerCode = response.json().get("retailerCode")
+            clean_retailerCode = raw_retailerCode.lower()
+
+            self.tokens_list.update({clean_retailerCode: token})
 
