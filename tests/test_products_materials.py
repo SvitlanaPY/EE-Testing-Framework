7a74d5132c3a7c8466bb1ff67960108938357f4f
Svitlana Pushchalo <svitlana8@gmail.com>
2026-06-23 01:03:12 +0300

    endpoint /products/materials

--- tests/test_products_materials.py
+++ tests/test_products_materials.py
@@ -9,7 +9,7 @@ class TestCity(BaseCase):
 
 
     @pytest.mark.parametrize('ZIP_Code, store_id, retailer', parametersList)
-    def test_response_structure(self, ZIP_Code, store_id, retailer):
+    def test_materials_response_structure(self, ZIP_Code, store_id, retailer):
 
         response = requests.get(f"{self.base_url}products/materials", params={'zipCode': ZIP_Code, 'storeId': store_id}, headers={"Authorization": self.tokens_list.get(retailer)})
         assert response.status_code == 200, 'Wrong status code'
@@ -18,13 +18,25 @@ class TestCity(BaseCase):
         assert len(response_as_dict) > 0, f"None material is returned"
 
         for key_name in self.json_keys:
-            assert key_name in response_as_dict[0], f'There is no "{key_name}" json key  in response'
+            assert key_name in response_as_dict[0], f'There is no "{key_name}" json key  in response.'
 
 
     @pytest.mark.parametrize('ZIP_Code, store_id, retailer', parametersList)
-    def test_(self, ZIP_Code, store_id, retailer):
+    def test_get_colors(self, ZIP_Code, store_id, retailer):
 
         response = requests.get(f"{self.base_url}products/materials", params={'zipCode': ZIP_Code, 'storeId': store_id}, headers={"Authorization": self.tokens_list.get(retailer)})
         assert response.status_code == 200, 'Wrong status code'
 
         response_as_dict = response.json()
+        for i in range(len(response_as_dict)):
+            assert len(response_as_dict[i]['colors']) > 0, f"None color is returned."
+
+
+
+    # @pytest.mark.parametrize('ZIP_Code, store_id, retailer', parametersList)
+    # def test_sort_materials_by_rank(self, ZIP_Code, store_id, retailer):
+    #     response = requests.get(f"{self.base_url}products/materials", params={'zipCode': ZIP_Code, 'storeId': store_id}, headers={"Authorization": self.tokens_list.get(retailer)})
+    #     assert response.status_code == 200, 'Wrong status code'
+    #
+    #     response_as_dict = response.json()
+
