9d517c55e332b8d9dc8575d966bef59a49abf746
Svitlana Pushchalo <svitlana8@gmail.com>
2026-08-30 01:09:39 +0300

    endpoint /retailer/cfis

--- tests/test_retailer_cfis.py
+++ tests/test_retailer_cfis.py
@@ -3,6 +3,7 @@ import requests
 from lib.base_case import BaseCase
 from .data_retailer_cfis import parametersList
 from .data_retailer_cfis import parametersList_ValidAddress
+from .data_retailer_cfis import parametersList_inValidAddress
 
 
 class TestCity(BaseCase):
@@ -77,16 +78,37 @@ class TestCity(BaseCase):
                     else:
                         deliveryStore_storeID_wAddress = cfi_w_address.get('deliveryStore')['storeId']
                     if deliveryStore_storeID_noAddress == deliveryStore_storeID_wAddress:
-                        assert cfi_no_address['distance'] != cfi_w_address['distance'], "Defining project's installation address does not affect the distance to the CFI."
+                        assert cfi_no_address['distance'] != cfi_w_address['distance'], "Defining the project's installation address does not affect the distance to the CFI."
                     break
 
-    # @pytest.mark.parametrize('ZIP_Code, store_id, prodQteGrp_ID, retailer', parametersList)
-    # def test_distance_is_unchanged_by_wrong_address(self, ZIP_Code, store_id, prodQteGrp_ID, retailer):
-    #     response = requests.get(f"{self.base_url}retailer/cfis", params={'zipCode': ZIP_Code, 'storeId': store_id, 'prodQteGrpId': prodQteGrp_ID},
-    #                             headers={"Authorization": self.tokens_list.get(retailer)})
-    #     assert response.status_code == 200, 'Wrong status code'
-    #
-    #     response_as_dict = response.json()
+    @pytest.mark.parametrize('ZIP_Code, store_id, prodQteGrp_ID, address, city, state, retailer', parametersList_inValidAddress)
+    def test_distance_not_changed_by_wrong_address(self, ZIP_Code, store_id, prodQteGrp_ID, address, city, state, retailer):
+        response_no_address = requests.get(f"{self.base_url}retailer/cfis",
+                                           params={'zipCode': ZIP_Code, 'storeId': store_id, 'prodQteGrpId': prodQteGrp_ID},
+                                           headers={"Authorization": self.tokens_list.get(retailer)})
+        assert response_no_address.status_code == 200, 'Wrong status code'
+
+        cfis_without_address = response_no_address.json()
+
+        response_with_address = requests.get(f"{self.base_url}retailer/cfis",
+                                             params={'zipCode': ZIP_Code, 'storeId': store_id, 'prodQteGrpId': prodQteGrp_ID, 'address': address, 'city': city, 'state': state},
+                                             headers={"Authorization": self.tokens_list.get(retailer)})
+        assert response_with_address.status_code == 200, 'Wrong status code'
+        cfis_with_address = response_with_address.json()
+
+        for cfi_no_address in cfis_without_address:
+            if cfi_no_address.get('deliveryStore') is None:
+                deliveryStore_storeID_noAddress = ""
+            else:
+                deliveryStore_storeID_noAddress = cfi_no_address.get('deliveryStore')['storeId']
+            for cfi_w_address in cfis_with_address:
+                if cfi_w_address['companyId'] == cfi_no_address['companyId']:
+                    if cfi_w_address.get('deliveryStore') is None:
+                        deliveryStore_storeID_wAddress = ""
+                    else:
+                        deliveryStore_storeID_wAddress = cfi_w_address.get('deliveryStore')['storeId']
+                    assert deliveryStore_storeID_noAddress == deliveryStore_storeID_wAddress or cfi_no_address['distance'] == cfi_w_address['distance'], "Defining the invalid project's installation address does affect the distance to the CFI."
+                    break
     #
     # @pytest.mark.parametrize('ZIP_Code, store_id, prodQteGrp_ID, retailer', parametersList)
     # def test_bad_zip(self, ZIP_Code, store_id, prodQteGrp_ID, retailer):
