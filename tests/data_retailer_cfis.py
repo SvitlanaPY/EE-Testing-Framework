109f21d1eb6bc098bc6681ffcb9ca8fe36cdfcd1
Svitlana Pushchalo <svitlana8@gmail.com>
2026-08-29 20:13:16 +0300

    testing data

---
+++ tests/data_retailer_cfis.py
@@ -0,0 +1,7 @@
+parametersList = [
+    ("30030", 28059, 720, "cliqstudios"),
+    ("85333", 28059, 720, "cliqstudios"),
+    ("30152", 28059, 720, "cliqstudios"),
+    ("85333", 28059, 740, "cliqstudios"),
+    ("30152", 28059, 740, "cliqstudios"),
+]
