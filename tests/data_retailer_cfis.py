01f57cd9a5c77498290322e0cf2ff482cd78c101
Svitlana Pushchalo <svitlana8@gmail.com>
2026-08-29 22:40:25 +0300

    testing data

--- tests/data_retailer_cfis.py
+++ tests/data_retailer_cfis.py
@@ -1,7 +1,17 @@
 parametersList = [
-    ("30030", 28059, 720, "cliqstudios"),
-    ("85333", 28059, 720, "cliqstudios"),
-    ("30152", 28059, 720, "cliqstudios"),
-    ("85333", 28059, 740, "cliqstudios"),
-    ("30152", 28059, 740, "cliqstudios"),
+    ("80001", 28059, 720, 300, "cliqstudios"),
+    ("30152", 28059, 720, 300, "cliqstudios"),
+    ("50050", 28059, 720, 300, "cliqstudios"),
+    ("85333", 28059, 720, 300, "cliqstudios"),
+    ("71601", 28059, 720, 300, "cliqstudios"),
+    ("86426", 28059, 720, 300, "cliqstudios"),
+    ("84601", 28059, 720, 300, "cliqstudios"),
+    ("43001", 26129, 720, 300, "flooranddecor"),
+    ("50050", 26267, 720, 300, "flooranddecor"),
+    ("78880", 26128, 720, 300, "flooranddecor"),
+    ("86426", 26048, 720, 300, "flooranddecor")
+]
+
+parametersList_WithAddress = [
+    ("30030", 28059, 720, "2308 East Lake Dr", "Decatur", "GA", "cliqstudios")
 ]
