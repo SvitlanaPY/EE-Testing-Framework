d4d16d0221363051ddef6d0c18c5c17bf2d24669
Svitlana Pushchalo <svitlana8@gmail.com>
2026-09-08 16:53:42 +0300

    endpoint /lookup/retailer

--- tests/test_lookup_retailer.py
+++ tests/test_lookup_retailer.py
@@ -14,3 +14,14 @@ class TestLookupRetailer(BaseCase):
         response_as_dict = response.json()
         assert response_as_dict['apiHostname'] == expected_api_HostName, f"Expected apiHostname: {expected_api_HostName}, but got: {response_as_dict['apiHostname']}"
         assert response_as_dict['retailerCode'] == expected_retailer_Code, f"Expected retailerCode: {expected_retailer_Code}, but got: {response_as_dict['retailerCode']}"
+
+
+    def test_lookup_retailer_invalid_clientHost(self):
+        invalid_clientHost = "invalid.clienthost.com"
+        response = requests.get(f"{self.base_url}lookup/retailer", params={'clientHost': invalid_clientHost})
+
+        assert response.status_code == 200, f"Expected status code 200 for invalid clientHost, but got {response.status_code}"
+
+        response_as_dict = response.json()
+        assert response_as_dict['apiHostname'] == None, f"Expected apiHostname: None, but got: {response_as_dict['apiHostname']}"
+        assert response_as_dict['retailerCode'] == None, f"Expected retailerCode: None, but got: {response_as_dict['retailerCode']}"
