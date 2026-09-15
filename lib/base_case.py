9e24bdd0d4ec859257dd2386e1af95ac434686ce
Svitlana Pushchalo <svitlana8@gmail.com>
2026-09-15 16:12:33 +0300

    endpoint /setup

--- lib/base_case.py
+++ lib/base_case.py
@@ -11,15 +11,44 @@ class BaseCase:
     tokens_list = {}
 
     def setup_class(self):
-        # retailer = {'clientHost': 'fd.staging.inscyth.com'}
-        #
-        # response = requests.get("https://ee-api-ssi.staging.inscyth.com/lookup/retailer", params = retailer)
-        # assert response.status_code == 200, 'Wrong status code'
-        #
-        # assert "token" in response.json(), "There is no Bearer token in the response_"
-        # self.token = response.json().get("token")
 
-        retailers = ['flooranddecor.staging.myprojectestimates.com', 'homeoutlet.staging.myprojectestimates.com', 'cliqstudios.staging.myprojectestimates.com', 'thertastore.staging.myprojectestimates.com', 'cabinetstogo.staging.myprojectestimates.com', 'lowes.staging.myprojectestimates.com']
+        # retailers = [
+        #     'bjs.staging.myprojectestimates.com',
+        #     'cabinetstogo.staging.myprojectestimates.com',
+        #     'cliqstudios.staging.myprojectestimates.com',
+        #     'flooranddecor.staging.myprojectestimates.com',
+        #     'flooringliquidators.staging.myprojectestimates.com',
+        #     'homeoutlet.staging.myprojectestimates.com',
+        #     'lowes.staging.myprojectestimates.com',
+        #     'lowesime.staging.myprojectestimates.com'
+        #     'thertastore.staging.myprojectestimates.com',
+        #     'wholesalecabinets.staging.myprojectestimates.com',
+        #     'bjs.staging.inscyth.com',
+        #     'cabinetstogo.staging.inscyth.com',
+        #     'cliqstudios.staging.inscyth.com',
+        #     'fd.staging.inscyth.com',
+        #     'flooringliquidators.staging.inscyth.com',
+        #     'homeoutlet.staging.inscyth.com',
+        #     'lowes.staging.inscyth.com',
+        #     'lowesime.staging.inscyth.com',
+        #     'thertastore.staging.inscyth.com',
+        #     'wholesalecabinets.staging.inscyth.com'
+        # ]
+
+        retailers = [
+            'bjs.qa.myprojectestimates.com',
+            'cabinetstogo.qa.myprojectestimates.com',
+            'cliqstudios.qa.myprojectestimates.com',
+            'flooranddecor.qa.myprojectestimates.com',
+            'flooringliquidators.qa.myprojectestimates.com',
+            'homeoutlet.qa.myprojectestimates.com',
+            'lowes.qa.myprojectestimates.com',
+            'lowesime.qa.myprojectestimates.com',
+            'lowesime.qa.myprojectestimates.com',
+            'thertastore.qa.myprojectestimates.com',
+            'wholesalecabinets.qa.myprojectestimates.com',
+            'fd.qa.inscyth.com'
+        ]
 
         for item in retailers:
             response = requests.get(f"{self.base_url}lookup/retailer", params={'clientHost': item})
