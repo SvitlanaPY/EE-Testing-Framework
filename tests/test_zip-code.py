53f62719e7dab4afb8267f38becb52082718f3ca
Svitlana Pushchalo <svitlana8@gmail.com>
2026-09-15 15:30:02 +0300

    endpoint /zip-code

--- tests/test_zip-code.py
+++ tests/test_zip-code.py
@@ -8,14 +8,13 @@ from .data_zipCode import parametersListNegative
 # Latitude is specified in degrees within the range [-90, 90].
 # Longitude is specified in degrees within the range [-180, 180).
 # 422: "Validation Error"
-
 class TestZipCode(BaseCase):
     # def setup_method(self):
     #     self.headers_ = {
     #         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjI5MTEsImlhdCI6MTY5ODg1NTM5NywibmJmIjoxNjk4ODU1Mzk3LCJleHAiOjE3MzAzOTEzOTd9.A8ns_cKXjPHcMupLeJddePhdkYhwStzmuwYSgwdG5FY"
     #     }
     @pytest.mark.parametrize("lat, long, expected_zipCode, expected_statusCode, retailer", parametersList)
-    def test_get_zipCode_by_latitude_longitude(self, lat, long, expected_zipCode, expected_statusCode, retailer):
+    def test_zipCode_get_zipcode_by_latitude_longitude(self, lat, long, expected_zipCode, expected_statusCode, retailer):
         response = requests.get(f"{self.base_url}zip-code", params={'latitude': lat, 'longitude': long}, headers={"Authorization": self.tokens_list.get(retailer)})
         assert response.status_code == expected_statusCode, 'Wrong status code'
 
