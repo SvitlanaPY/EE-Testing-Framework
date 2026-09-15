94b20ddc37f3eccc5292dcba80ace5231c00d35e
Svitlana Pushchalo <svitlana8@gmail.com>
2026-09-15 16:12:45 +0300

    testing data

--- tests/data_products_materials.py
+++ tests/data_products_materials.py
@@ -2,8 +2,11 @@
 
 parametersList = [
     ("76180", 25977, "flooranddecor"),
+    ("07652", 25922, "fd"),
     ("30152", 28059, "cliqstudios"),
     ("47025", 21885, "lowes"),
+    ("77701", 23006, "lowesime"),
     ("07450", 26200, "homeoutlet"),
-    ("85333", 25970, "thertastore")
+    ("85333", 25970, "thertastore"),
+    ("30030", 28060, "wholesalecabinets")
 ]
